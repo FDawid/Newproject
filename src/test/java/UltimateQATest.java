e922d025fc4012814ff007ec46962adb9712ed4c
FDawid <dawidfeluks@gmail.com>
2023-03-19 17:01:15 +0100

    Project created

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -1,23 +1,38 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // modyfikatory dotępu w Java
 // różnica pomiędzy klasą, interfejsem a klasą abstrakcyjną
-public class UltimateQATest {
+public class UltimateQATest extends PageSetup{
 
-    String projectLocation = System.getProperty("user.dir");
-    String ultimateQAURL = "https://ultimateqa.com/simple-html-elements-for-automation/";
-    // słowo kluczowe void - zwracane z funkcji
-    // słowo kluczowe static
     @Test
-    public void testOne() {
-        //biblioteka WebdriverManager - io.github.boniegarcia
-    System.setProperty("webdriver.chrome.driver", projectLocation + "/resources/chromedriver.exe");
+   public void testOne() {
+        WebElement button = driver.findElement(By.id("idExample"));
+        button.click();
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(ultimateQAURL);
+        WebElement buttonSucess = driver.findElement(By.className("entry-title"));
+        //import static
+        assertEquals ( "Button success", buttonSucess.getText());
+    }
+
+    @Test
+    public void testTwo() {
+        WebElement nameInput = driver.findElement(By.id("et_pb_contact_name_0"));
+        nameInput.sendKeys("Tomasz");
+
+        WebElement emailInput = driver.findElement(By.id("et_pb_contact_email_0"));
+        nameInput.sendKeys("Tomasz@tester.pl");
+
+        WebElement  thanksText = driver.findElement(By.xpath("//div[@class='et-pb-contact-message']/p"));
+
 
     }
 }
